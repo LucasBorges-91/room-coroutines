44ce7506acfcaf295655bf60cc96c6599d3e3f25
Lucas Borges <lucas.pborges@yahoo.com.br>
2022-06-28 17:37:50 -0300

    criando o banco de dados

---
+++ app/src/main/java/br/com/douglasmotta/naivagtioncomponentappmirror/data/db/AppDatabase.kt
@@ -0,0 +1,32 @@
+package br.com.douglasmotta.naivagtioncomponentappmirror.data.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database( entities = [UserEntity::class], version = 1 )
+abstract class AppDatabase : RoomDatabase() {
+
+    companion object {
+        // padrão singleton para previnir a criação de várias instancias do database ao mesmo tempo
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase( context: Context ) : AppDatabase {
+            val tempInstance = INSTANCE
+            if ( tempInstance != null ) {
+                return tempInstance
+            }
+            synchronized( this ) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
