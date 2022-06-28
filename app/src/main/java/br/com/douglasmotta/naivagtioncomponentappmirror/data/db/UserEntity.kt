aa10df032b5a607ce2e55a95c6f228c365323b02
Lucas Borges <lucas.pborges@yahoo.com.br>
2022-06-28 17:29:02 -0300

    iniciando projeto

---
+++ app/src/main/java/br/com/douglasmotta/naivagtioncomponentappmirror/data/db/UserEntity.kt
@@ -0,0 +1,16 @@
+package br.com.douglasmotta.naivagtioncomponentappmirror.data.db
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user")
+data class UserEntity (
+    @PrimaryKey
+    val id: Long = 0,
+    val name: String,
+    val bio: String,
+    val username: String,
+    val password: String
+)
+
+
