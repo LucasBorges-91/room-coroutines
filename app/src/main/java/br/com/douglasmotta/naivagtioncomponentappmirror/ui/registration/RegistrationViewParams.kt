32a66971152874b175d8cc5de76ff27f04952912
Lucas Borges <lucas.pborges@yahoo.com.br>
2022-06-28 18:28:58 -0300

    criando o repositório e as classes necessarias para sua implementacao

---
+++ app/src/main/java/br/com/douglasmotta/naivagtioncomponentappmirror/ui/registration/RegistrationViewParams.kt
@@ -0,0 +1,8 @@
+package br.com.douglasmotta.naivagtioncomponentappmirror.ui.registration
+
+data class RegistrationViewParams (
+    var name: String = "",
+    var username: String = "",
+    var bio: String = "",
+    var password: String = ""
+)
